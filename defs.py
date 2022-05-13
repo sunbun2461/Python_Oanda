064aaf6abae2f5acddaa330b995ec1c00f3a97bf
Thomas A Rio <thomasario@Thomass-MacBook-Pro.local>
2022-05-12 22:07:06 -0400

    first commit

---
+++ defs.py
@@ -0,0 +1,7 @@
+API_KEY = "a979ea3f8ae1b550f5bba07d332bc958-3e44128eb6860513f954e110755ee4eb"
+ACCOUNT_ID = "101-001-13555201-002"
+OANDA_URL = 'https://api-fxpractice.oanda.com/v3'
+
+SECURE_HEADER = {
+    'Authorization': f'Bearer {API_KEY}'
+}
