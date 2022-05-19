6c563fd0ef8c1868c42fa6dd4874d5c24de0c3a1
Thomas A Rio <thomasario@Thomass-MacBook-Pro.local>
2022-05-19 01:42:19 -0400

    new candle data, new ma_sim.py simulation and all_trades.pkl result.

--- instrument.py
+++ instrument.py
@@ -11,8 +11,8 @@ class Instrument():
             10, ob['pipLocation'])  # -4 -> .0001
         self.marginRate = ob['marginRate']
 
-    # def __repr__(self):
-    #     return str(vars(self))
+    def __repr__(self):
+        return str(vars(self))
 
     @classmethod
     def get_instruments_df(cls):
@@ -23,6 +23,36 @@ class Instrument():
         df = cls.get_instruments_df()
         return [Instrument(x) for x in df.to_dict(orient='records')]
 
+    # ...
+    # dict to access instruments by pair name
+
+    # instrument_dict =
+    # {
+    #     "EUR_USD" : Instrument(),
+    #     "EUR_GBP" : Instrument(),
+    #     "..." : Instrument(),
+    # }
+
+    # our_instrument = instrument_dict["EUR_USD"]
+    # ...
+
+    @classmethod
+    def get_instruments_dict(cls):
+        i_list = cls.get_instruments_list()
+        i_keys = [x.name for x in i_list]
+        return { k:v for (k,v) in zip(i_keys, i_list) }
+
+
+    @classmethod
+    def get_instrument_by_name(cls,pairname):
+        d = cls.get_instruments_dict()
+        if pairname in d: 
+            return d[pairname]
+        else:
+            return None
+
 
 if __name__ == "__main__":
-    print(Instrument.get_instruments_list())
+    # for k, v in Instrument.get_instruments_dict().items():
+    #     print(k, v)
+    print(Instrument.get_instrument_by_name('EUR_USD'))
